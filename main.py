75dd628eef6922eb25377bef58f9cf13d5ae69c4
abhi.misc244 <abhi.mic244@gmail.com>
2016-10-29 14:19:36 +0800

    Added comments

--- main.py
+++ main.py
@@ -11,12 +11,22 @@ from kivy.clock import Clock
 from graphtheory import Graph
 from functools import partial
 
-class GraphInterface(Widget):
+'''
+This is a Kivy project to for Graph Theory. This creates nodes and edges.
+The nodes and edges can be added using the buttons. Nodes must be created first (atleast two).
+Edges are created by selecting two nodes by double click. Nodes move by drag and drop style movement. Edges adjust according to the movement of Nodes
+'''
 
+
+'''
+This is the main class that refers to other classes such as GraphNode and GraphEdge.
+'''
+class GraphInterface(Widget):
+    #Global variable that tracks all edges
     global edge_matrix
     edge_matrix = []
-    'testing'
 
+    #Global variable that tracks all nodes
     global node_matrix
     node_matrix = []
 
@@ -24,11 +34,8 @@ class GraphInterface(Widget):
     def createNode(self, instance):
         global node_matrix
 
-        '''Creating a GraphNode Class. Using ObjectProperty for a shell reference'''
+        '''Creating a GraphNode Class.'''
         node = GraphNode(pos = (500,500))
-
-        print len(node_matrix)
-        print node
 
         '''Creating a Node Matrix. Will use this matrix in APP Class for future calculations'''
         node_matrix.append(node)
@@ -41,95 +48,109 @@ class GraphInterface(Widget):
         node_matrix[-1].text = 'Button'+ str(len(node_matrix))
         print node_matrix[-1].text
 
-
-
-
-        '''
-        vertex = GraphNode(pos = (500,500))
-        #print self.node_matrix
-        self.node_matrix.append(vertex)
-        self.graph.add_vertex(vertex)
-
-        self.node_matrix[-1].text = 'Button'+ str(len(self.node_matrix))
-        game.add_widget(self.node_matrix[-1])
-        print "Node Created"
-        print self.graph
-        '''
-
     def createEdge(self, instance):
+        '''Creating a GraphEdge Class.'''
+
+        '''Getting reference to the global node_matrix for tracking all nodes'''
         global node_matrix
-        print "Edge Created"
+
+        '''Setting initial values of Node1 and Node2 to None. Not sure if required.
+        To be investigated'''
         node2 = None
         node1 = None
 
+        '''A for loop to get two nodes that are selected by double clicking. These nodes should
+        have a changed colour in user interface.'''
         for i in range (0, len(node_matrix)):
             if (node_matrix[i].selected) and (node1 == None):
                 node1 = node_matrix[i]
             if (node_matrix[i].selected) and (node_matrix[i] != node1):
                 node2 = node_matrix[i]
 
+        '''Creating a generic edge called 'c'. Addign that generic edge to the main game'''
         c = GraphEdge(node1, node2)
-        #self.graph.add_edge(edge)
-
-        print "Edge changed"
-        #self.edge_matrix.append(c)
         self.add_widget(c)
 
+        #Future - for integration with graph class
+        #self.graph.add_edge(edge)
+        #self.edge_matrix.append(c)
         #print self.graph
 
-
-
 class GraphApp(App):
+    '''Generic Kivy Application'''
 
     def build(self):
 
+        '''Creating an instance of GraphInterface. This is the main class where all other class
+        are created.'''
         game = GraphInterface()
+
+        '''Creating an instance of Graph class. This class is part of future project.'''
         self.graph = Graph()
 
+        '''Creating the two buttons that are to be used for creating nodes and edges'''
         createNodeButton = Button(text = 'CreateNode', pos=(100,0))
         createEdgeButton = Button(text = 'CreateEdge')
         game.add_widget(createNodeButton)
         game.add_widget(createEdgeButton)
 
+        '''Referencing the actions of the buttons'''
         createNodeButton.bind(on_press=game.createNode)
         createEdgeButton.bind(on_press=game.createEdge)
-        #Clock.schedule_interval(self.update, 1.0/60)
+
+        '''
+        For a time based update. Not used in this case
+        #Clock.schedule_interval(self.update, 1.0/60)'''
+
         return game
 
-
-
-
+    '''
+    For a time based update. Not used in this case
     def update(self, dt):
-        self.edge.update_edge([100,100,300,300])
+        self.edge.update_edge([100,100,300,300])'''
 
 
 
 class GraphNode(Widget):
+    '''The Node creating Class'''
+
     def __init__(self, **kwargs):
         super(GraphNode, self).__init__(**kwargs)
+
+        '''This creates a circle at specified position initally. The node can then be dragged to
+        different position. '''
         with self.canvas:
             Color(1,1,1)
             self.object = Ellipse(pos=(500,500))
 
+        '''Creating a label for the node represented by ellipse. Inital text will be New Node.
+        Once its dragged from the original position, its name changes based on when its created'''
         self.l = Label(text='New Node', color=[1,0,1,1], size=(200,200),pos=self.pos)
         self.add_widget(self.l)
         self.text = "New Node"
 
+        '''Any change to the position of the node will trigger update_object function'''
         self.bind(pos=self.update_object)
+
+        '''This variable is used to get connection points of the edges'''
         self.selected = 0
 
 
     def update_object(self, *args):
+        '''Ellipse position is set as widget position'''
         self.object.pos = self.pos
+
+        '''The lable position is set as widget position'''
         self.l.pos = self.pos
         self.l.text = self.text
+
+        '''Had to do this as label kept dissapearing whenever canvas was cleared.
+        Not sure why this was happening. to be investigated'''
         self.remove_widget(self.l)
         self.add_widget(self.l)
-        #self.l.color = [1,0,1,1]
-
-
 
     def on_touch_down(self,touch):
+        '''Colour changing effect when a node is selected'''
         if self.collide_point(*touch.pos) and touch.is_double_tap:
             self.selected = not self.selected
             if self.selected:
@@ -138,7 +159,6 @@ class GraphNode(Widget):
                     Color(1,0,1)
                     self.object = Ellipse(pos=(self.pos))
                     self.update_object()
-
             elif not self.selected:
                 with self.canvas:
                     self.canvas.clear()
@@ -146,18 +166,19 @@ class GraphNode(Widget):
                     self.object = Ellipse(pos=(self.pos))
                     self.update_object()
 
-
-
         elif self.collide_point(*touch.pos):
+            '''For dragging the node to new position. Grab is a kivy function'''
             touch.grab(self)
             print 'grabed item'
 
     def on_touch_move(self, touch):
+        '''For dragging the node to new position. Grab is a kivy function'''
         if touch.grab_current is self:
             self.pos =  (touch.x, touch.y)
 
 
     def on_touch_up(self, touch):
+        '''For dragging the node to new position. Grab is a kivy function'''
         if touch.grab_current is self:
             print "ungrabed a button"
             touch.ungrab(self)
@@ -165,51 +186,39 @@ class GraphNode(Widget):
 
 
 class GraphEdge(Widget):
+    '''The Edge creating class. This needs two nodes as inputs.'''
+
     def __init__(self,node1, node2, **kwargs):
         super(GraphEdge, self).__init__(**kwargs)
         self.node1 = node1
         self.node2 = node2
+
+        '''This is the name of the edge'''
         self.text = 'Edge--'+node1.text + '-' + node2.text
+
+        '''The points list of the edge.'''
         self.points = list(node1.center) + list(node2.center)
-        print self.text
+
+        '''Drawing a line between the two nodes given with class creation'''
         with self.canvas:
             Color(1,1,1)
             self.object = Line(points=self.points, width=3)
 
-
-        node1.bind(pos=self.update_object)
-        node2.bind(pos=self.update_object)
-
-        print 'the line points are ------------', self.object.points
-
-        #print self.properties()
-
-
-
+        '''Whenever anoy of the nodes position changes, this triggers update_object function'''
+        self.node1.bind(pos=self.update_object)
+        self.node2.bind(pos=self.update_object)
 
     def update_object(self, node, pos):
-        print 'con property changed'
-        print 'changed pos is ', pos
-        print  'node1 pos is ', self.node1.pos
-        print  'node2 pos is ', self.node2.pos
+        '''With the bind function, this function gets the node object and the position of the node as inputself. '''
 
+        '''Reassign the values of points to match the new position of node'''
         self.points = list(self.node1.center) + list(self.node2.center)
 
-
-
-        #self.canvas.clear()
-
-        #new = list(node1pos) + list(node2pos)
+        '''Clear the canvas. Redraw the line between unmoved node and moved node'''
         with self.canvas:
             self.canvas.clear()
             Color(1,1,1)
             self.object= Line(points=self.points, width=3)
-        print 'the line points are ------------', self.points
-
-
-
-
-
     pass
 
 if __name__ == '__main__':
